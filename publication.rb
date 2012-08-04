e883339e4842f7a80b73bc134a8d29de708cdfb0
Alice Bartlett <alice.bartlett@gmail.com>
2012-08-04 11:23:15 +0100

    Initial commit

---
+++ publication.rb
@@ -0,0 +1,57 @@
+require 'sinatra'
+require 'json'
+
+get '/edition/' do
+  require './space_parser'
+  @checkin_time = "#{Time.now().strftime('%l%P')}"
+  
+  if params["test"]
+    etag Digest::MD5.hexdigest("test"+Time.now.getutc.to_s)
+    @count, @message, @date = SpaceParser::fetch_data()
+  else
+    etag Digest::MD5.hexdigest(Time.now.strftime('%l%p'))
+    
+    err_count = 0
+    while @space_data.nil?
+      begin
+        @space_data = SpaceParser::fetch_data(token)
+      rescue NetworkError
+        err_count +=1
+        if err_count > 2
+          return 502
+        end
+      rescue PermanentError
+        err_count +=1
+        if err_count > 2
+          return 500
+        end
+      rescue Exception => e
+        puts e.message
+        puts e.backtrace
+        err_count +=1
+        if err_count > 2
+          return 500
+        end
+      end
+    end
+  end
+  if params["delivery_count"] == "0"
+    erb :welcome
+  elsif Time.now - @date < 1.day 
+    erb :welcome
+  end
+end
+
+post '/validate_config/' do
+  content_type :json
+  response = {}
+  response[:valid] = true
+  response.to_json
+end
+
+
+get '/sample/' do
+  require './instagram_parser'
+  @space_data = SpaceParser::SAMPLE_DATA
+  erb :edition
+end
---
+++ space_parser.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'net/https'
+require 'open-uri'
+
+require 'feedzirra'
+class SpaceParser
+    
+  def self.fetch_data()        
+    feed = Feedzirra::Feed.fetch_and_parse("http://feeds.feedburner.com/SpaceRightNow")
+    entry = feed.entries.first
+    count = entry.title
+    message = entry.summary
+    date = entry.published
+    return count, message, date
+  end                                
+end
+
+class PermanentError < StandardError; end
+class NetworkError < StandardError; end
